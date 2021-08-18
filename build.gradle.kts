1b7b074a8e0ec06798e8a872817e2cc530145b23
Andrey Mukamolov <fobo66@protonmail.com>
2021-08-18 21:55:31 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    `java-gradle-plugin`
+
+    kotlin("jvm") version "1.5.21"
+    id("com.gradle.plugin-publish") version "0.12.0"
+}
+
+group = "dev.fobo66.secretsloader"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
+
+pluginBundle {
+    website = "https://github.com/fobo66/secretsLoader"
+    vcsUrl = "https://github.com/fobo66/secretsLoader.git"
+    tags = listOf("android", "secrets", "config", "credentials-management")
+}
+
+gradlePlugin {
+    val secretsLoader by plugins.creating {
+        id = "dev.fobo66.secretsloader"
+        displayName = "SecretsLoader – load secrets from the"
+        description =
+            "A plugin that helps you with loading sensitive data like API keys from encrypted YAML files into variant-specific res values or build config fields"
+        implementationClass = "dev.fobo66.secretsloader.SecretsLoaderPlugin"
+    }
+}
+
+val functionalTestSourceSet = sourceSets.create("functionalTest") {
+}
+
+gradlePlugin.testSourceSets(functionalTestSourceSet)
+configurations.getByName("functionalTestImplementation").extendsFrom(configurations.getByName("testImplementation"))
+
+val functionalTest by tasks.creating(Test::class) {
+    testClassesDirs = functionalTestSourceSet.output.classesDirs
+    classpath = functionalTestSourceSet.runtimeClasspath
+}
+
+val check by tasks.getting(Task::class) {
+    dependsOn(functionalTest)
+}
---
+++ src/main/kotlin/dev/fobo66/secretsloader/SecretsLoaderPlugin.kt
@@ -0,0 +1,10 @@
+package dev.fobo66.secretsloader
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class SecretsLoaderPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        TODO("Not yet implemented")
+    }
+}
