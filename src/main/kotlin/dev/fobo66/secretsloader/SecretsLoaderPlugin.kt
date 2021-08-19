7e16aacbcacfbcf58df926b47bb594faa08232c2
Andrey Mukamolov <fobo66@protonmail.com>
2021-08-19 22:45:00 +0300

    Added initial code for traversing variants

--- src/main/kotlin/dev/fobo66/secretsloader/SecretsLoaderPlugin.kt
+++ src/main/kotlin/dev/fobo66/secretsloader/SecretsLoaderPlugin.kt
@@ -1,11 +1,15 @@
 package dev.fobo66.secretsloader
 
+import com.android.build.api.variant.AndroidComponentsExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import com.android.build.AndroidComponentsExtension
 
 class SecretsLoaderPlugin : Plugin<Project> {
     override fun apply(target: Project) {
-        target.extensions.register("secretsLoader", SecretsLoaderExtension::class.java)
+        target.extensions.create("secretsLoader", SecretsLoaderExtension::class.java)
+
+        target.extensions.findByType(AndroidComponentsExtension::class.java)?.beforeVariants {
+
+        }
     }
 }
