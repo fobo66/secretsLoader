f8ee45c9778d56b7a64233048759185f59ee3c82
Andrey Mukamolov <fobo66@protonmail.com>
2021-08-19 23:10:08 +0300

    Added initial version of the task

---
+++ src/main/kotlin/dev/fobo66/secretsloader/LoadSecretsTask.kt
@@ -0,0 +1,12 @@
+package dev.fobo66.secretsloader
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+class LoadSecretsTask: DefaultTask() {
+
+    @TaskAction
+    fun loadSecrets() {
+
+    }
+}
