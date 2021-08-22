a6418c5c379da279f2e75da245352ee2692e9ae2
Andrey Mukamolov <amv@bamboogroup.eu>
2021-08-22 12:49:51 +0300

    Integrate task

--- src/main/kotlin/dev/fobo66/secretsloader/SecretsLoaderExtension.kt
+++ src/main/kotlin/dev/fobo66/secretsloader/SecretsLoaderExtension.kt
@@ -1,5 +1,6 @@
 package dev.fobo66.secretsloader
 
+import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.provider.Property
 
 abstract class SecretsLoaderExtension {
@@ -20,13 +21,24 @@ abstract class SecretsLoaderExtension {
     abstract val encryptionPassword: Property<String>
 
     /**
+     * Message digest algorithm for the secrets file
+     */
+    abstract val encryptionMessageDigestAlgorithm: Property<String>
+
+    /**
      * Suffix for the secrets file that will be removed after decryption. Used commonly to distinguish encrypted files
      */
     abstract val encryptionSuffix: Property<String>
 
+    /**
+     * Suffix for the secrets file that will be removed after decryption. Used commonly to distinguish encrypted files
+     */
+    abstract val secretsFolder: DirectoryProperty
+
     init {
         encryptionToolExecutable.convention("openssl")
         encryptionAlgorithm.convention("aes-256-cdc")
+        encryptionMessageDigestAlgorithm.convention("md5")
         encryptionSuffix.convention(".cipher")
     }
 }
--- src/main/kotlin/dev/fobo66/secretsloader/SecretsLoaderPlugin.kt
+++ src/main/kotlin/dev/fobo66/secretsloader/SecretsLoaderPlugin.kt
@@ -3,13 +3,22 @@ package dev.fobo66.secretsloader
 import com.android.build.api.variant.AndroidComponentsExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.register
 
 class SecretsLoaderPlugin : Plugin<Project> {
     override fun apply(target: Project) {
-        target.extensions.create("secretsLoader", SecretsLoaderExtension::class.java)
+        val secretsParams = target.extensions.create("secretsLoader", SecretsLoaderExtension::class.java)
 
         target.extensions.findByType(AndroidComponentsExtension::class.java)?.beforeVariants {
+            val loadSecretsTask = target.tasks.register("load${it.name}Secrets", LoadSecretsTask::class) {
+                encryptionAlgorithm.set(secretsParams.encryptionAlgorithm)
+                encryptionMessageDigestAlgorithm.set(secretsParams.encryptionMessageDigestAlgorithm)
+                encryptionPassword.set(secretsParams.encryptionPassword)
+                secretInputs.set(secretsParams.secretsFolder)
+                secretOutputs.set(target.layout.buildDirectory.dir("secrets"))
+            }
 
+            target.tasks.findByName("assemble${it.name}")?.dependsOn(loadSecretsTask)
         }
     }
 }
