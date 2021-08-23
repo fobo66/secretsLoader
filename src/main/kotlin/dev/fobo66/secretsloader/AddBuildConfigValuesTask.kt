8b1d05fb61e4892f009accafff895be19e3e94e5
Andrey Mukamolov <amv@bamboogroup.eu>
2021-08-24 00:53:33 +0300

    Initial version of the build config task

---
+++ src/main/kotlin/dev/fobo66/secretsloader/AddBuildConfigValuesTask.kt
@@ -0,0 +1,10 @@
+package dev.fobo66.secretsloader
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+class AddBuildConfigValuesTask : DefaultTask() {
+
+    @TaskAction
+    fun addBuildConfigValues() {}
+}
---
+++ src/test/kotlin/dev/fobo66/secretsloader/AddBuildConfigValuesTaskTest.kt
@@ -0,0 +1,15 @@
+package dev.fobo66.secretsloader
+
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+
+class AddBuildConfigValuesTaskTest {
+
+    @BeforeTest
+    fun setUp() {
+    }
+
+    @Test
+    fun addBuildConfigValues() {
+    }
+}
