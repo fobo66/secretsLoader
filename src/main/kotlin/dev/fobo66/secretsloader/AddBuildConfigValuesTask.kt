b5548b70b39aad7ea4da07bff3ce753d253624df
Andrey Mukamolov <amv@bamboogroup.eu>
2021-08-24 16:26:53 +0300

    Added logic to load YML files to the build config

--- src/main/kotlin/dev/fobo66/secretsloader/AddBuildConfigValuesTask.kt
+++ src/main/kotlin/dev/fobo66/secretsloader/AddBuildConfigValuesTask.kt
@@ -1,10 +1,37 @@
 package dev.fobo66.secretsloader
 
+import com.android.build.api.variant.AndroidComponentsExtension
+import dev.fobo66.secretsloader.util.SecretsProcessor
 import org.gradle.api.DefaultTask
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.TaskAction
+import org.gradle.kotlin.dsl.newInstance
+import javax.inject.Inject
 
-class AddBuildConfigValuesTask : DefaultTask() {
+abstract class AddBuildConfigValuesTask @Inject constructor(objectFactory: ObjectFactory) : DefaultTask() {
+
+    @get:Input
+    abstract val flavorName: Property<String>
+
+    @get:InputFile
+    abstract val buildConfigFile: RegularFileProperty
+
+    private val secretsProcessor = objectFactory.newInstance(SecretsProcessor::class)
 
     @TaskAction
-    fun addBuildConfigValues() {}
+    fun addBuildConfigValues() {
+        project.extensions.findByType(AndroidComponentsExtension::class.java)?.let { androidComponents ->
+            val variantSelector = androidComponents.selector().withName(flavorName.get())
+            androidComponents.onVariants(variantSelector) {
+                val buildConfigSecrets =
+                    secretsProcessor.loadBuildConfigValues(buildConfigFile.asFile.get().inputStream())
+
+                it.buildConfigFields.putAll(buildConfigSecrets)
+            }
+        }
+    }
 }
