0d7cee00b6f266108383dcf9ab0be2a5612ce677
Andrey Mukamolov <amv@bamboogroup.eu>
2021-09-10 14:38:25 +0300

    Added tests for signing config loading

---
+++ src/test/kotlin/dev/fobo66/secretsloader/AddSigningConfigTaskTest.kt
@@ -0,0 +1,67 @@
+package dev.fobo66.secretsloader
+
+import com.android.build.api.dsl.ApplicationExtension
+import dev.fobo66.secretsloader.util.SECRETS_DIR_NAME
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.getByName
+import org.gradle.kotlin.dsl.register
+import org.gradle.testfixtures.ProjectBuilder
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+
+internal class AddSigningConfigTaskTest {
+
+    private lateinit var project: Project
+
+    @BeforeTest
+    fun setUp() {
+        project = ProjectBuilder.builder().build()
+        project.mkdir(SECRETS_DIR_NAME)
+        project.mkdir("build/$SECRETS_DIR_NAME")
+        val secretsFile = project.layout.buildDirectory.dir(SECRETS_DIR_NAME).get()
+            .file(SECRET_FILE).asFile
+        val testSecretsBytes =
+            this.javaClass.classLoader.getResourceAsStream(SECRET_FILE)!!.readAllBytes()
+        secretsFile.writeBytes(testSecretsBytes)
+        project.pluginManager.apply("com.android.application")
+        project.extensions.getByName<ApplicationExtension>("android").let {
+            it.compileSdk = 30
+            it.defaultConfig {
+                applicationId = "com.example.test"
+                minSdk = 30
+                targetSdk = 30
+            }
+
+            it.buildTypes {
+                getByName("debug") {}
+            }
+        }
+    }
+
+    @Test
+    fun addSigningConfig() {
+        val addSigningConfigTask =
+            project.tasks.register<AddSigningConfigTask>("addDebugSigningConfig") {
+                signingConfigFile.set(project.layout.buildDirectory.dir(SECRETS_DIR_NAME).get().file(
+                    SECRET_FILE
+                ))
+                keystoreFile.set(project.layout.buildDirectory.dir(SECRETS_DIR_NAME).get().file(
+                    SECRET_FILE
+                ))
+                buildType.set(BUILD_TYPE)
+            }
+
+        addSigningConfigTask.get().addSigningConfig()
+
+
+        project.extensions.getByName<ApplicationExtension>("android").let { android ->
+            assertNotNull(android.signingConfigs.findByName(BUILD_TYPE))
+        }
+    }
+
+    companion object {
+        private const val SECRET_FILE = "signingConfig.yml"
+        private const val BUILD_TYPE = "test"
+    }
+}
